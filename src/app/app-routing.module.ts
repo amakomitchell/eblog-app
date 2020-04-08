7f74cac1f68aa826caa7c2a594863de406dad42a
Ugochi <amakomitchell5@gmail.com>
2020-04-08 22:47:15 +0100

    styling on the registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { AuthGuard } from './core/interceptors';
 
 const routes: Routes = [
   { path: '#', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: '#/login', component: LoginComponent },
-  { path: '#/register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 
   // otherwise redirect to home
   { path: '#', redirectTo: '' }
