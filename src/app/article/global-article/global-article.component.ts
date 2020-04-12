326db1ed24c282e19805253d0598619b95dded12
Ugochi <amakomitchell5@gmail.com>
2020-04-12 18:47:04 +0100

    displayed global feed

--- src/app/article/global-article/global-article.component.ts
+++ src/app/article/global-article/global-article.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { first } from 'rxjs/operators';
 import { ArticleService } from 'src/app/core/services/article.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-global-article',
