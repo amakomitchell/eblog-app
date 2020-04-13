469399ea5883986c55ab4a40e35c7664fb9efd2a
Ugochi <amakomitchell5@gmail.com>
2020-04-13 15:32:28 +0100

    added footer

--- src/app/article/global-article/global-article.component.ts
+++ src/app/article/global-article/global-article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ArticleService } from 'src/app/core/services/article.service';
 
@@ -14,12 +15,17 @@ export class GlobalArticleComponent implements OnInit {
     limit: 5
   };
 
+  // pager = {};
+  // pageOfItems = [];
+
   constructor(
-    private articleService: ArticleService
+    private articleService: ArticleService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.loadGlobalArticles();
+
   }
 
   private loadGlobalArticles() {
@@ -28,5 +34,13 @@ export class GlobalArticleComponent implements OnInit {
     this.articleService.getArticles(offset, limit)
     .subscribe(articles => this.articles = articles.articles);
   }
+
+//   private loadPage(page) {
+//     // get page of items from api
+//     this.http.get<any>(`/api/items?page=${page}`).subscribe(x => {
+//         this.pager = x.pager;
+//         this.pageOfItems = x.pageOfItems;
+//     });
+// }
 
 }
