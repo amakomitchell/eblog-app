3d9666aebf5447f8828b967f7a10a09ff8b2ec87
Ugochi <amakomitchell5@gmail.com>
2020-04-17 13:52:10 +0100

    dynamic copyright year

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  anio: number = new Date().getFullYear();
 
   constructor() { }
 
