f8b39ed0a96de7ba8a0ea7515327e6a2d09d49f0
Ugochi <amakomitchell5@gmail.com>
2020-04-17 23:09:52 +0100

    wrote test for favourite-button component

--- src/app/shared/favorite-button/favorite-button.component.spec.ts
+++ src/app/shared/favorite-button/favorite-button.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoriteButtonComponent } from './favorite-button.component';
 
+import { By } from '@angular/platform-browser';
+
 describe('FavoriteButtonComponent', () => {
   let component: FavoriteButtonComponent;
   let fixture: ComponentFixture<FavoriteButtonComponent>;
@@ -16,10 +18,22 @@ describe('FavoriteButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FavoriteButtonComponent);
     component = fixture.componentInstance;
+
+    component.favoritesCount = 1;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call #click with correct params when "FavouritesCount" is clicked', () => {
+    const  navigateSpy = spyOn(component,  'onClick');
+
+    const anchorList = fixture.debugElement.queryAll(By.css('.button-count'));
+    anchorList[0].nativeElement.dispatchEvent(new CustomEvent('click'));
+    
+    expect(navigateSpy).toHaveBeenCalledWith(false, 1);
+  })
 });
